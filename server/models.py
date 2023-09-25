990a378a427020c36e2f4d3e94f377b88ce03604
Hiroki Kato <hirokikato1@gmail.com>
2023-09-25 14:15:59 -0700

    updated metadata convention

--- server/models.py
+++ server/models.py
@@ -1,6 +1,90 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
+
+
+class Planet(db.Model, SerializerMixin):
+    __tablename__ = 'planets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.Integer)
+    nearest_star = db.Column(db.String)
+
+    # Add relationship
+    missions = db.relationship(
+        'Mission', backref='planet')
+    # Add serialization rules
+    serialize_rules = ("-missions.planet",)
+
+
+class Scientist(db.Model, SerializerMixin):
+    __tablename__ = 'scientists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+
+    # Add relationshipmis
+    missions = db.relationship(
+        'Mission', backref='scientist')
+    # Add serialization rules
+    serialize_rules = ("-missions.scientist",)
+    # Add validation
+
+    @validates('name', 'field_of_study')
+    def validate_name(self, db_column, value):
+        if db_column == 'name':
+            if type(value) == str and len(value) > 0:
+                return value
+            else:
+                raise ValueError('A scientist needs a name of type string')
+        elif db_column == 'field_of_study':
+            if type(value) == str and len(value) > 0:
+                return value
+            else:
+                raise ValueError(
+                    'A scientist needs a field of study that is of type string')
+
+
+class Mission(db.Model, SerializerMixin):
+    __tablename__ = 'missions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    # Add relationships
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    # Add serialization rules
+    serialize_rules = ("-planet.missions", "-scientist.missions")
+    # Add validation
+
+    @validates('name')
+    def validate_name(self, db_column, name):
+        if name and len(name) > 0:
+            return name
+        else:
+            raise ValueError('A mission must have a name')
+
+    @validates('scientist_id')
+    def validate_scientist_id(self, db_column, scientist_id):
+        scientist = Scientist.query.filter(Scientist.id == scientist_id)
+        if not scientist or scientist_id == None:
+            raise ValueError('A mission must have a valid scientist id')
+        else:
+            return scientist_id
+
+    @validates('planet_id')
+    def validate_planet_id(self, db_column, planet_id):
+        planet = Planet.query.filter(Planet.id == planet_id)
+        if not planet or planet_id == None:
+            raise ValueError('A mission must have a valid planet id')
+        else:
+            return planet_id
+
+
+# add any models you may need.
